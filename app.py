940effc812200de5ee068888a0c8b69960bb202f
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-02 12:14:35 +0530

    func

--- app.py
+++ app.py
@@ -6,4 +6,22 @@ import re,string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
-def 
+
+app = Flask(__name__)
+
+def remove_pattern(text,pattern):
+    reg = re.findall(pattern,text)
+    for pat in reg:
+        text = re.sub(pat,"",text)
+    return text
+
+def count_punct(text):
+    count = sum([1 for char in text if char in string.punctuation ])
+    return round(count/(len(text) - text.count(" ")),3)*100
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
