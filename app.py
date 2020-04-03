35805fc185b0d09440081375a7f6114f70280aa8
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-03 08:02:29 +0530

    routes

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 from flask import Flask, render_template,url_for,request
-import pandas as pd 
-import numpy as np 
-import nltk
-from nltk.stem.porter import PorterStemmer
-import re,string 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score,accuracy_score,roc_auc_score
-import warning
 
-
-app = Flask(__name__)
-
-def remove_pattern(text,pattern):
+def patternremove(text,pattern):
     reg = re.findall(pattern,text)
     for pat in reg:
         text = re.sub(pat,"",text)
@@ -23,9 +10,28 @@ def count_punct(text):
     count = sum([1 for char in text if char in string.punctuation ])
     return round(count/(len(text) - text.count(" ")),3)*100
 
-## Model creation
 
-data = pd.read_csv("sentiment.csv")
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/predict")
+def predict:
+    if request.method == "POST":
+        msg = request.form["message"]
+        data = [msg]
+        vect = pd.DataFrame(cv.transform(data).toarray())
+        body_len = pd.DataFrame([len(data)- data.count(" ")])
+        punct = pd.DataFrame([count_punct(data)])
+        total_data = pd.concat([body_len,punct,vect],axis=1)
+        pred = log.predict(total_data)
+        return render_template("predict.html",pred=pred)
+
+
 
 
 
