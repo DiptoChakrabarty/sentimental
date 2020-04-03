a1c36d9d618a8feca231f5a5e537337ad7b7f280
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-03 08:53:37 +0530

    bad predictions

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template,url_for,request
+from sklearn.externals import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import string
+from sentiment import vector
 
 def patternremove(text,pattern):
     reg = re.findall(pattern,text)
@@ -19,16 +24,20 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/predict")
-def predict:
+@app.route("/predict",methods=["POST"])
+def predict():
     if request.method == "POST":
+        cv = vector()
         msg = request.form["message"]
         data = [msg]
         vect = pd.DataFrame(cv.transform(data).toarray())
         body_len = pd.DataFrame([len(data)- data.count(" ")])
         punct = pd.DataFrame([count_punct(data)])
         total_data = pd.concat([body_len,punct,vect],axis=1)
+
+        log = joblib.load("model.pkl")
         pred = log.predict(total_data)
+        print(pred)
         return render_template("predict.html",pred=pred)
 
 
--- sentiment.py
+++ sentiment.py
@@ -6,7 +6,8 @@ import re,string
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split,cross_val_score,GridSearchCV
-from sklearn.metrics import f1_score,accuracy_score,roc_auc_score
+from sklearn.metrics import f1_score,accuracy_score,roc_auc_score,confusion_matrix
+from sklearn.externals import joblib
 
 #Classification Models import 
 from sklearn.ensemble import RandomForestClassifier
@@ -19,6 +20,7 @@ from sklearn.svm import SVC
 
 
 ##function defination
+vocabulary = ['and', 'document', 'first', 'is', 'one', 'second', 'the', 'third', 'this']
 
 def patternremove(text,pattern):
     reg = re.findall(pattern,text)
@@ -29,6 +31,10 @@ def patternremove(text,pattern):
 def count_punct(text):
     count = sum([1 for char in text if char in string.punctuation ])
     return round(count/(len(text) - text.count(" ")),3)*100
+
+def vector():
+    vectorizer = CountVectorizer(stop_words="english",vocabulary=vocabulary)
+    return vectorizer
 
 ## Model creation
 data = pd.read_csv("sentiment.csv",sep="\t")
@@ -59,7 +65,8 @@ data["percentage"] = data["body"].apply(lambda x: count_punct(x))
 words = " ".join([sent for sent in data["tidy_text"]])
 
 ## Count Vectorizer 
-vectorizer = CountVectorizer(stop_words="english")
+
+vectorizer =vector()
 vect = vectorizer.fit_transform(data["tidy_text"])
 inputdata = pd.concat([data["length"],data["percentage"],pd.DataFrame(vect.toarray())],axis=1)
 print(inputdata.head())
@@ -87,20 +94,52 @@ for mod,clf in model:
     print("Model is {} and Score is {}".format(mod,scores.mean()))
 '''
 #Hyper tuning
-'''
-param_grid = {"C" : [0.001,0.01,0.1]}
-grid = GridSearchCV(LogisticRegression(),param_grid,cv=5)
+
+
+'''param_grid = {"C" : [0.001,0.01,0.1,1,10]}
+grid = GridSearchCV( LogisticRegression(),param_grid,cv=5)
 grid.fit(inputdata,data["label"])
 
 print(grid.best_estimator_)'''
 
-log = LogisticRegression(C=0.1)
+'''log = LogisticRegression(C=0.1)
 scores = cross_val_score(log,inputdata,data["label"],scoring="accuracy",cv=3)
-print("Scores obtained is {}".format(scores))
+print("Scores obtained is {}".format(scores))'''
 
-X_train, X_test, y_train, y_test = train_test_split( inputdata,data["label"], test_size=0.33, random_state=42) 
+X_train, X_test, y_train, y_test = train_test_split( inputdata,data["label"], test_size=0.20, random_state=42) 
+log = LogisticRegression(C=0.001, class_weight=None, dual=False, fit_intercept=True,
+                   intercept_scaling=1, l1_ratio=None, max_iter=100,
+                   multi_class='auto', n_jobs=None, penalty='l2',
+                   random_state=None, solver='lbfgs', tol=0.0001, verbose=0,
+                   warm_start=False)
 
 log.fit(X_train,y_train)
-print(log.predict(X_test))
+pred =log.predict(X_test)
+print(accuracy_score(pred,y_test))
+print(confusion_matrix(pred,y_test))
+
+
+'''param_grid = {"n_estimators" : [5,10,30,100]}
+grid = GridSearchCV( RandomForestClassifier(),param_grid,cv=5)
+grid.fit(inputdata,data["label"])
+
+print(grid.best_estimator_)
+forest = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
+                       criterion='gini', max_depth=None, max_features='auto',
+                       max_leaf_nodes=None, max_samples=None,
+                       min_impurity_decrease=0.0, min_impurity_split=None,
+                       min_samples_leaf=1, min_samples_split=2,
+                       min_weight_fraction_leaf=0.0, n_estimators=5,
+                       n_jobs=None, oob_score=False, random_state=None,
+                       verbose=0, warm_start=False)
+
+forest.fit(X_train,y_train)
+pred =forest.predict(X_test)
+print(accuracy_score(pred,y_test))
+print(confusion_matrix(pred,y_test))'''
+
+
+
+joblib.dump(log,"model.pkl")
 
 
